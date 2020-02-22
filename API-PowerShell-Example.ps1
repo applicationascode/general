7943ad3dddab5f9caaeb4fc931ca94ff54e11be8
applicationascode <61347438+applicationascode@users.noreply.github.com>
2020-02-22 10:57:09 +0100

    Update API-PowerShell-Example.ps1

--- API-PowerShell-Example.ps1
+++ API-PowerShell-Example.ps1
@@ -1,5 +1,5 @@
-$ip = Invoke-RestMethod -uri http://ipinfo.io/json | Select -exp ip
-$city = (Invoke-RestMethod -uri "https://api.ipgeolocation.io/ipgeo?apiKey=00718fd5c84d4f8e9c86c7b0f885e3ee&ip=$ip").city
+$ip = Invoke-RestMethod -uri http://ipinfo.io/json -Method GET | Select -exp ip
+$city = (Invoke-RestMethod -uri "https://api.ipgeolocation.io/ipgeo?apiKey=00718fd5c84d4f8e9c86c7b0f885e3ee&ip=$ip" -Method GET).city
 $weer = (Invoke-RestMethod -uri "http://vdnborn.nl:1337/getweer?locatie=$city" -Method GET).liveweer
 $temp = $weer.temp
 
